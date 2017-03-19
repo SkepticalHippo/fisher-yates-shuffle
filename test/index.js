591d206d6ceb0ece531121e5e832749ff84e6c8a
Edvinas <edvinasrp@gmail.com>
2017-03-19 13:44:24 +0000

    added randomizer test

--- test/index.js
+++ test/index.js
@@ -12,4 +12,19 @@ describe('shuffle()', function() {
         assert.notDeepEqual(deck, shuffledDeck);
         assert.equal(deck.length, shuffledDeck.length);
     });
+
+    it('should accept a randomizer function', function() {
+        const deck = Array.apply(null, {length: 52}).map(function(value, index) {
+            return index;
+        });
+
+        const random = function() {
+            return 0.323;
+        };
+
+        const shuffledDeck = shuffle(deck, random);
+
+        assert.notDeepEqual(deck, shuffledDeck);
+        assert.equal(deck.length, shuffledDeck.length);
+    });
 });
